0cec8154bc905346be26c0ba9bda8adab3d6fb11
AlokDugar <alokdugar4@gmail.com>
2025-03-20 15:31:38 +0545

    success messages within dashboard

--- resources/views/dashboard/users.blade.php
+++ resources/views/dashboard/users.blade.php
@@ -4,7 +4,14 @@
 
 @section('content')
 <div class="content container-fluid">
-
+    @if(session('success'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        {{ session('success') }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    @endif
     <!-- Page Header -->
     <div class="page-header">
         <div class="row align-items-center">
